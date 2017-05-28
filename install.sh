2f127bd73220695e19a4ab89b3e012141c4712ff
linux52 <linux52@tom.com>
2017-05-28 23:23:38 +0800

    Delete install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-apt-get update -y && apt-get install -y python-pip wget python vim net-tools iputils-ping curl virt-what zip git nmap;
-sleep 3;
-pip install -y python-memcached requests beautifulsoup poster;
-sleep 3;
-pip install --upgrade pip;
